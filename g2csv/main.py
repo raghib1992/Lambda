97a9c1f42f5db703ab07471578a759e72a671922
raghib1992 <raghib.npti@gmail.com>
2020-11-06 20:39:51 +0530

    added g2csv updated version

--- g2csv/main.py
+++ g2csv/main.py
@@ -1,5 +1,19 @@
 import json
 import requests
+from bs4 import BeautifulSoup
 
-r = requests.get("https://www.facebook.com")
-print(r)
+
+def main(event,context):
+    response = []
+    r = requests.get("https://www.google.com/search?q=" + event['keyword'])
+    bs = BeautifulSoup(r.text, features="html.parser")
+    for link in bs.find_all('div', {'class': 'g'}):
+        title = link.find('a')
+        url = link.find('cite')
+        if title and url:
+            response.append({'title':title.text,'url':url.text})
+    return json.dumps(reponse,sort_keys=True, indent=4)
+
+
+event = json.loads('{"Keyword"}:{"docker"}')
+print main(event,"")
